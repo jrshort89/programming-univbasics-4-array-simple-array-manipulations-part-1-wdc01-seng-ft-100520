231b18240597bb924232183cba7311cd159f3619
Jacob Short <jrshort89@gmail.com>
2020-08-31 22:30:09 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,22 @@ def using_push(array, string)
  array.push(string)
 end
 
+def using_unshift(array, string)
+  array.unshift(string)
+end
+
+def using_pop(array)
+  array.pop()
+end
+
+def pop_with_args(array, num)
+  array.pop(num)
+end
+
+def using_shift(array)
+  array.shift()
+end
+
+def shift_with_args(array, num)
+  array.shift(num)
+end
